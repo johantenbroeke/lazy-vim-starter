f41904644fabd636a7dfedc19edb51cfa4074e29
Johan ten Broeke <johan.tenbroeke@emesa.nl>
2024-09-05 10:41:14 +0200

    return the table for cmp plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,33 +1,33 @@
 -- ~/.config/nvim/lua/plugins/cmp.lua
 
--- Import the cmp setup function
-local cmp = require("cmp")
-
-cmp.setup({
-  mapping = {
-    -- Custom mappings for navigation
-    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    
-    -- Set <C-y> to confirm the selected completion
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item with Ctrl-y
-
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Also confirm with Enter
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  -- Other cmp configurations...
-})
+return {
+  -- Specify the 'nvim-cmp' plugin to be configured
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      -- Customize the key mappings for 'nvim-cmp'
+      local cmp = require("cmp")
+      opts.mapping = cmp.mapping.preset.insert({
+        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Confirm with Ctrl-y
+        -- ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- Confirm with Enter
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      })
+    end,
+  }
+}
 
